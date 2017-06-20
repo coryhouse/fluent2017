22caa59fd79c4d216bdb47c4eee05ad907531a30
Cory House <housecor@gmail.com>
2017-06-20 07:06:00 -0700

    Switch to Jest

--- src/math.test.js
+++ src/math.test.js
@@ -1,9 +1,8 @@
-import {expect} from 'chai';
 import {add} from './math';
 
 describe('add', () => {
   it('should add two integers', () => {
     const result = add(2,3);
-    expect(result).to.equal(5);
+    expect(result).toEqual(5);
   });
 });
