cf7b813df1e30583b82f19db258b5199c565fe5c
Cory House <housecor@gmail.com>
2017-06-20 07:34:10 -0700

    Switch to Mocha

--- src/math.test.js
+++ src/math.test.js
@@ -1,8 +1,9 @@
+import {expect} from 'chai';
 import {add} from './math';
 
 describe('add', () => {
   it('should add two integers', () => {
     const result = add(2,3);
-    expect(result).toEqual(5);
+    expect(result).to.equal(5);
   });
 });
